18d876e6399a2650f2000cb333e22e221dc67b7e
Nikolas McPhail <facadebhop@gmail.com>
2022-08-24 16:03:37 -0400

    removing wrong files

--- program2.py
+++
@@ -1,9 +0,0 @@
-numberOne = int(input("Please give the first number: "))
-numberTwo = int(input("Please give the second number: "))
-
-numberRange = range(numberOne, numberTwo)
-for n in numberRange:
-    if n % 7 == 0:
-        if n % 5 != 0:
-            print(n, end=" ")
-
